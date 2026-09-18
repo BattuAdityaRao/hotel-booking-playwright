bc7942c95be51a1ed04b48ca9432f91f474624e1
heycharitha <charithaarigela@ibm.com>
2026-09-18 12:42:33 +0530

    Complete admin room management automation

--- pages/LoginPage.js
+++ pages/LoginPage.js
@@ -1,9 +1,22 @@
 export class LoginPage {
   constructor(page) {
     this.page = page;
+
+    this.adminLink = page.getByText('Admin', { exact: true });
+
+    this.usernameInput = page.locator('#username');
+    this.passwordInput = page.locator('#password');
+    this.loginButton = page.locator('#doLogin');
   }
 
   async navigate() {
-    await this.page.goto('/#/admin');
+    await this.page.goto('/');
+    await this.adminLink.click();
+  }
+
+  async login(username, password) {
+    await this.usernameInput.fill(username);
+    await this.passwordInput.fill(password);
+    await this.loginButton.click();
   }
 }
--- playwright.config.js
+++ playwright.config.js
@@ -26,17 +26,26 @@ export default defineConfig({
     },
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      use: {
+        ...devices['Desktop Chrome'],
+        storageState: 'playwright/.auth/admin.json',
+      },
       dependencies: ['setup'],
     },
     {
       name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
+      use: {
+        ...devices['Desktop Firefox'],
+        storageState: 'playwright/.auth/admin.json',
+      },
       dependencies: ['setup'],
     },
     {
       name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
+      use: {
+        ...devices['Desktop Safari'],
+        storageState: 'playwright/.auth/admin.json',
+      },
       dependencies: ['setup'],
     },
   ],
